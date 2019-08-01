aea5a031c5d209218c42d1e2226d7161720d7745
Esther <estherariya427@gmail.com>
2019-08-01 15:37:40 -0700

    Pokemon list added on js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-alert('Hello world');
+var repository = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Ivysaur',
+    height: 10,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Venusaur',
+    height: 20,
+    types: ['grass', 'poison']
+  }
+];
+
+for(var i = 0; i < repository.length; i++)
+{
+  document.write("<p>" + repository[i].name);
+  document.write(" (height: " + repository[i].height + ")");
+  if(repository[i].height >= 20)
+  {
+    document.write(" - Wow, that's big!")
+  }
+}
