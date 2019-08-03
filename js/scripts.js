4ccb1c4283c0dc029cdb83aeece8042f5dff5574
Esther <estherariya427@gmail.com>
2019-08-03 01:44:55 -0700

    Update scripts.js
    Added forEach function to loop through repository array to display the details

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ var repository = [
   }
 ];
 
-for(var i = 0; i < repository.length; i++)
+/* for(var i = 0; i < repository.length; i++)
 {
   document.write("<p>" + repository[i].name);
   document.write(" (height: " + repository[i].height + ")");
@@ -24,4 +24,12 @@ for(var i = 0; i < repository.length; i++)
   {
     document.write(" - Wow, that's big!")
   }
-}
+} */
+
+// Using forEach function to loop over Pokemon in the repository array
+repository.forEach(function(property) {
+  document.write('<h3>==============</h3>');
+  Object.keys(property).forEach(function(currentItem) {
+    document.write('<p>' + currentItem + ': ' + property[currentItem] + '</p>');
+  })
+});
